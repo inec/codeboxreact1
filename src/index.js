ad3775630e59646956ea18f2907caecad007d168
ine <inec@users.noreply.github.com>
2018-09-05 00:41:34 -0500

    fun

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import React from "react";
+import ReactDOM from "react-dom";
+import { SkiDayCount } from "./components/SkiDayCount";
 
-window.React = React
+import "./styles.css";
 
-render(
-	<SkiDayCount />,
-	document.getElementById('react-container')
-)
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <SkiDayCount />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
