035fa0c6b06214f2237730e027f616efb53196a1
ine <inec@users.noreply.github.com>
2018-09-05 00:46:54 -0500

    ff

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { SkiDayCount } from "./components/SkiDayCount";
 
-import "./styles.css";
+import "./stylesheets/ui.scss";
+import "./stylesheets/globals.scss";
+import "./stylesheets/index.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <SkiDayCount />
     </div>
   );
