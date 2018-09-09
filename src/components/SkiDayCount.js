d6fd370b098cfb8901a938b89319839cde3c7b31
inec <luyege+github@gmail.com>
2018-09-09 00:16:38 -0500

    'test'

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,48 +1,41 @@
-//import {Component} from 'react'
 import '../stylesheets/ui.scss'
 import {MdTerrain} from 'react-icons/md/'
 import  {TiWeatherSnow} from 'react-icons/ti/'
 import { FiCalendar}   from 'react-icons/fi/'
 
-const percentToDecimal=(decimal)=>{
-	return((decimal * 100 )+'%')
+
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + '%')
 }
-/*percentToDecimal(decimal){
-    return((decimal * 100 )+'%')
-}*/
-const calcGoalProgress=(total,goal)=>{
+
+const calcGoalProgress = (total, goal) => {
 	return percentToDecimal(total/goal)
 }
-/*calcGoalProgress(total,goal){
-	return this.percentToDecimal(total/goal)
-}*/
 
-//export const SkiDayCount  extends Component{
-
-export const SkiDayCount =({total,powder,backcountry,goal})=>(
-	//render() {
-		//return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{total}</span>
-				<FiCalendar />
-					<span>dayss</span>
-				</div>
-				<div className="powder-days">
-					<span>{powder}</span>
-					<TiWeatherSnow />
-					<span>dasy</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{backcountry}</span>
-					<MdTerrain />
-					<span>sday</span>
-				</div>
-				<div>
-					<span>{calcGoalProgress(
-						total,goal)} </span>
-				</div>
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+		<div className="ski-day-count">
+			<div className="total-days">
+				<span>{total}</span>
+					<FiCalendar />
+				<span>days</span>
 			</div>
-		)
-	//}
-//}
+			<div className="powder-days">
+				<span>{powder}</span>
+					<TiWeatherSnow />
+				<span>days</span>
+			</div>
+			<div className="backcountry-days">
+				<span>{backcountry}</span>
+					<MdTerrain />
+				<span>days</span>
+			</div>
+			<div>
+				<span>
+					{calcGoalProgress(
+						total, 
+						goal
+					)}
+				</span>
+			</div>
+		</div>
+)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList} from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount total={51}
-				 powder={21}
-				 backcountry={10}
-				 goal={100}/>,
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw Valley",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirkwood",
+				date: new Date("3/28/2016"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mt. Tallac",
+				date: new Date("4/2/2016"),
+				powder: false,
+				backcountry: true
+			}
+		]
+	} 
+	/>
+
+				 ,
 	document.getElementById('react-container')
 )
